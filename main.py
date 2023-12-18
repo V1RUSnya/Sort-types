d9e3bf69e0de5d40ab2fc46c9dd0ec2db4c70bd6
Hexqueller <dima.nesed@gmail.com>
2023-12-18 22:35:48 +0300

    Дебаг
    Ну и фикс само собой

--- main.py
+++ main.py
@@ -3,39 +3,45 @@ import random
 import time
 
 def test(arr):
-    for i in range(len(arr)-1):
-        if arr[i]>arr[i+1]:
+    for i in range (len(arr)-1):
+        if arr[i] > arr[i+1]:
             return False
-        return True
+    return True
 
 def input_arr_randomize():
     arr = []
     random.seed()
-    for i in range (10**2): #Колво элементов
+    for i in range (10): # Кол-во элементов
         arr.append(random.randint(1,100)) #Диапозон
     return arr
-
 
 sol = metod.Solution()
 formats = "-"*100
 inputs = input_arr_randomize()
 
+debug_select = None # Дебаг
 print(f"{formats}\nМассив: {inputs}\n{formats}\nКаким методом сортировать?\n0) Выход\n1) Пузырьком\n2) Сортировка выбором")
 while True:
-    select = int(input())
-    # select = 2 # Debug
+    if debug_select == None:
+        select = int(input())
+    else:
+        select = debug_select
     start_time = time.time()
     if select == 0:
         break
     elif select == 1:
         result = sol.bubble(inputs)
     elif select == 2:
-        result = sol.Sort_viborom(inputs)
+        result = sol.selection(inputs)
+
     end_time = time.time()
-    execution_time = end_time - start_time
-    if test(result):
+    execution_time = end_time - start_time # Время
+
+    if test(result) == True:
         print(f"{result}\nВремя: {round(execution_time,5)} секунды")
         inputs = input_arr_randomize()
     else:
         print("Ошибка в работе!")
+        if debug_select != None:
+            print(f"Информация для отладки: {result}")
         break
--- metod.py
+++ metod.py
@@ -1,24 +1,24 @@
 class Solution():
-    def bubble(self,arr: list)->list:
-        a=0
-        while a<= len(arr):
-            for i in range (len(arr)-1-a):
-                if arr[i]>arr[i+1]:
+    def bubble(self, arr: list) -> list:
+        a = 0
+        while a < len(arr):
+            for i in range(len(arr) - 1 - a):
+                if arr[i] > arr[i+1]:
                     temp = arr[i+1]
                     arr[i+1] = arr[i]
                     arr[i] = temp
-            a+=1
+            a += 1
         return arr
     
-    def selection(self,arr: list)->list:
-        min,a = 0,0
-        while a <= len(arr)-1:
-            for i in range (1+a,len(arr)):
-                if arr[i]<arr[min]:
+    def selection(self, arr: list) -> list:
+        a = 0
+        while a <= len(arr) - 1:
+            min = a
+            for i in range(1 + a, len(arr)):
+                if arr[i] < arr[min]:
                     min = i
             temp = arr[a]
             arr[a] = arr[min]
             arr[min] = temp
-            a+=1
-            min = a
+            a += 1
         return arr
